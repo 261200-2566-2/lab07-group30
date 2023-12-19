2528c9dff823aea73ea6d1410058a75268e711fb
Chartf <90751426+Chartf@users.noreply.github.com>
2023-12-19 20:07:46 +0700

    add code

---
+++ src/HashSets.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+import java.util.Iterator;
+public class HashSets<E> implements Set<E>, Iterable<E> {
+    private HashMap<E, Object> map;
+    private static final Object PRESENT = new Object();
+
+    public HashSets() {
+        map = new HashMap<>();
+    }
+
+    @Override
+    public boolean add(E e) {
+        return map.put(e, PRESENT) == null;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return map.containsKey(o);
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return map.remove(o) != null;
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return map.keySet().iterator();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+}
---
+++ src/Set.java
@@ -0,0 +1,9 @@
+public interface Set<E> {
+
+    public boolean add(E e);
+    public boolean contains(Object o);
+    public int size();
+    public boolean remove(Object o);
+    public void clear();
+    public boolean isEmpty();
+}
